9dc45eeba245b324633d9a7b562953abb82c5234
shamilbi <shamilbi@gmail.com>
2025-10-12 18:13:30 +0200

    +ListProto2(ListProto), +List2(List)

--- radio_curses/curses_utils.py
+++ radio_curses/curses_utils.py
@@ -265,6 +265,58 @@ class List:
         return True
 
 
+class ListProto2(ListProto):
+    def record_move_up(self, i: int) -> bool:  # type: ignore[empty-body]
+        pass
+
+    def record_move_down(self, i: int) -> bool:  # type: ignore[empty-body]
+        pass
+
+
+class List2(List):
+    'List + move_up(), move_down()'
+
+    def __init__(
+        self,
+        win: curses.window,
+        proto: ListProto2,
+        current_color: int = curses.A_BOLD,
+    ):
+        super().__init__(win, proto, current_color)
+        self.proto: ListProto2 = proto
+
+    def move_up(self, i: int):
+        if not self.proto.record_move_up(i):
+            return
+        if self.idx > 0:
+            self.idx -= 1
+        if self.cur > 0:
+            self.cur -= 1
+        self.refresh()
+
+    def move_down(self, i: int):
+        if not self.proto.record_move_down(i):
+            return
+        len_ = self.proto.records_len()
+        rows, _ = self.win.getmaxyx()
+        if self.idx < len_ - 1:
+            self.idx += 1
+        if self.cur < rows - 1:
+            self.cur += 1
+        self.refresh()
+
+    def handle_input(self, ch: int) -> bool:
+        if super().handle_input(ch):
+            return True
+        if ch == curses.KEY_SR:  # Shift-Up
+            self.move_up(self.idx)
+        elif ch == curses.KEY_SF:  # Shift-Down
+            self.move_down(self.idx)
+        else:
+            return False
+        return True
+
+
 class App:
     def __init__(self, screen):
         self.screen = screen
